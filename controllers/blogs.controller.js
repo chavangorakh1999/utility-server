5f1c90ccf874e42f946ea5044eedcc6f7aa2e066
GORAKH CHAVAN <chavangorakh19999@gmail.com>
2026-03-10 11:05:11 +0530

    Fixed blog scraping

--- controllers/blogs.controller.js
+++ controllers/blogs.controller.js
@@ -1,41 +1,37 @@
-const axios = require("axios");
 const cheerio = require('cheerio');
+const Parser = require('rss-parser');
 const ResponseCodes = require("../utils/response.code");
 
+const parser = new Parser({ customFields: { item: ['content:encoded'] } });
 let response_code = new ResponseCodes();
 let server_status = response_code.serverError().status;
 let success_status = response_code.success().status;
 
 const getBlogs = async (req, res) => {
-  let response = await axios.get(
-    "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@grcsr"
-  );
-  if (!response.data.items && response.data.items.length === 0) {
+  try {
+    const feed = await parser.parseURL("https://medium.com/feed/@grcsr");
+
+    if (!feed.items || feed.items.length === 0) {
+      response_code.message = "No blogs found.";
+      return res.status(server_status).send(response_code.serverError());
+    }
+
+    const blogs = feed.items.map((blog) => {
+      const $ = cheerio.load(blog['content:encoded'] || '');
+      const image = $('img').first().attr('src') || null;
+      const description = blog['content:encodedSnippet']?.split('\n').find(l => l.trim().length > 40) || null;
+
+      return { title: blog.title, description, image, url: blog.link };
+    });
+
+    response_code.message = "Blogs fetched successfully";
+    response_code.data = blogs;
+    return res.status(success_status).send(response_code.success());
+  } catch (error) {
     response_code.message = "Something went wrong - Please try again.";
     response_code.error = error;
     return res.status(server_status).send(response_code.serverError());
   }
-  let blogItems = response?.data?.items;
-  let scrappedBlogs = await Promise.all(
-    blogItems.map(async (blog) => {
-      const response = await axios.get(blog.link);
-      const htmlContent = response.data;
-
-      // Parse HTML content using cheerio
-      const $ = cheerio.load(htmlContent);
-
-      // Extract title, description, and image
-      const title = $("title").text();
-      const description = $('meta[name="description"]').attr("content");
-      const image = $('meta[property="og:image"]').attr("content");
-
-      // Return the extracted metadata
-      return { title, description, image,url:blog.link };
-    })
-  );
-  response_code.message = "Blogs fetched sucessfully";
-  response_code.data = scrappedBlogs;
-  return res.status(success_status).send(response_code.success());
 };
 
 module.exports = {
